02496a30afb7c396ca5b0cbbecbeb88fd12d482c
Rebecca Jensen <rebecca.jensen@nordstrom.com>
2018-10-10 15:33:27 -0700

    gulp basic example

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create()
+const useref = require('gulp-useref')
+const uglify = require('gulp-uglify')
+const gulpIf = require('gulp-if')
+const cssnano = require('gulp-cssnano')
+const imagemin = require('gulp-imagemin')
+const cache = require('gulp-cache')
+const del = require('del')
+const runSequence = require('run-sequence')
+
+//compiles scss to css
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+})
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+//live reloading
+gulp.task('watch', ['browserSync', 'sass'], function() {
+  gulp.watch('app/scss/**/*.scss', ['sass'])
+  gulp.watch('app/*.html', browserSync.reload)
+  gulp.watch('app/js/**/*.js', browserSync.reload)
+})
+
+//minifies js and css
+gulp.task('useref', function() {
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+})
+
+//minify images
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+//delete dist folder
+gulp.task('clean:dist', function() {
+  return del.sync('dist')
+})
+
+gulp.task('build', function(callback) {
+  runSequence('clean:dist', ['sass', 'useref', 'images'], callback)
+})
